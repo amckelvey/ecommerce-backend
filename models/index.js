cdbcc59ec185b66d221cd2608d488d6891928027
Majelvey <mckelvey.andy@gmail.com>
2022-06-16 17:31:37 -0400

    updated associations

--- models/index.js
+++ models/index.js
@@ -6,27 +6,29 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  
+  foreignKey: 'product_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-
-})
+  foreignKey: 'tag_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
+    unique: false,
   }
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    unique: false,
   }
-})
+});
 
 module.exports = {
   Product,
